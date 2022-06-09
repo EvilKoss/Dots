5abe960292b9099067b149cf4bab66ec126706b1
EViL-KoSS <evil.koss@gmail.com>
2022-06-09 21:35:01 +0300

    refact

--- scripts/script/GameScene.js
+++ scripts/script/GameScene.js
@@ -2,7 +2,7 @@ class GameScene extends Phaser.Scene {
     constructor() {
         super('Game');
     }
-
+    
     preload() {
         this.load.image('violet' , 'sprites/violetDot.png');
         this.load.image('blue' , 'sprites/blueDot.png');
@@ -10,7 +10,7 @@ class GameScene extends Phaser.Scene {
         this.load.image('red' , 'sprites/redDot.png');
         this.load.image('green' , 'sprites/greenDot.png');
     }
-
+    
     init() {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.score = 0;
@@ -38,47 +38,53 @@ class GameScene extends Phaser.Scene {
         this.createTable();
         this.createDots();
     }
-
-    pledge() {
-        if(this.memoryDict.length > 1){
-            let column = this.table.length -1;
-            this.memoryDict.forEach(item => {
-                this.table[item.y][item.x] = 9;
-                this.score++;
-            });
-            console.log(this.score);
-            while(column >= 0){
-                let i = this.table.length -1;
-                while(i >= 0){
-                    let j = i;
-                    while(j >= 0 && this.table[j][column] === 9 && j >= 0){
-                        j--;
-                        if(j === 0 && this.table[j][column] === 9){
-                            while(this.table[j][column] === 9){
-                                this.table[j][column] = Phaser.Math.Between(0, 4);
-                                j++;
-                            }
-                            break;
-                        }
-                    };
-                    if(j >= 0 && this.table[i][column] === 9 && this.table[j][column] != 9){
-                        this.table[i][column] = this.table[j][column];
-                        this.table[j][column] = 9;
-                    }else if(this.table[i][column] === 9 && i === 0){
-                        this.table[i][column] = Phaser.Math.Between(0, 4);
-                    }
-                    i--;
-                };
-                column--;
-            }
     
-            this.memoryDict = [];
-            this.dots.forEach(item => {item.destroy()});
-            this.dots = [];
-            this.createDots();
-        }else{return}
+    pledge() {
+        if(this.memoryDict.length < 1) return;
+        this.memoryDict.forEach(item => {
+            this.table[item.y][item.x] = 9;
+            this.score++;
+        });
+        console.log(this.score);
+        for(let x = 0; x < this.table.length; x++) {
+            while(true) {
+                //find hole
+                let hole = -1;
+                for(let y = this.table.length-1; y >= 0; y--) {
+                    if(this.table[y][x] === 9) {
+                        hole = y;
+                        break;
+                    }
+                }
+                if(hole < 0) {
+                    break;
+                }
+                //find ball
+                let ball = -1;
+                for(let y = hole-1; y >= 0; y--) {
+                    if(this.table[y][x] !== 9){
+                        ball = y;
+                        break;
+                    }
+                }
+                if(ball < 0) {
+                    break;
+                }
+                //move ball into hole
+                this.table[hole][x] = this.table[ball][x];
+                this.table[ball][x] = 9;
+            }
+            //randomize ramaining holes
+            for(let y = 0; y < this.table.length && this.table[y][x] === 9; y++) {
+                this.table[y][x] = Phaser.Math.Between(0, 4);
+            }
+        }
+        this.memoryDict = [];
+        this.dots.forEach(item => {item.destroy()});
+        this.dots = [];
+        this.createDots();
     }
-
+    
     createTable() {
         for(let i = 0; i < this.table.length; i++){
             for(let j = 0; j < this.table[i].length; j++){
@@ -86,12 +92,12 @@ class GameScene extends Phaser.Scene {
             }
         }
     }
-
+    
     onDotsClicked(i,j) {
         this.click = true;
         this.memoryDict = [{x: j, y: i}];
     }
-
+    
     onDotsOver(i,j) {
         if(this.click === true){
             let checkX = this.memoryDict[0].x;
@@ -104,21 +110,21 @@ class GameScene extends Phaser.Scene {
             }
         }
     }
-
+    
     onDotsUp() {
         this.click = false;
         this.pledge();
     }
-
+    
     createDots() {
         for(let i = 0; i < this.table.length; i++){
             for(let j = 0; j < this.table[i].length; j++){
                 let width = config.width / 2 + (50 * j - 150);
                 let height = config.height / 2 + (50 * i - 150);
                 const dot = this.add.graphics()
-                    .setInteractive(new Phaser.Geom.Circle(width,  height, 10), Phaser.Geom.Circle.Contains)
-                    .fillStyle(this.tableColors[this.table[i][j]])
-                    .fillCircle(width,  height, 10);
+                .setInteractive(new Phaser.Geom.Circle(width,  height, 10), Phaser.Geom.Circle.Contains)
+                .fillStyle(this.tableColors[this.table[i][j]])
+                .fillCircle(width,  height, 10);
                 dot.on("pointerdown", () => { this.onDotsClicked(i,j) });
                 dot.on("pointerover", () => { this.onDotsOver(i,j) });
                 this.dots.push(dot);
@@ -126,5 +132,5 @@ class GameScene extends Phaser.Scene {
         }
         this.input.on("pointerup", () => { this.onDotsUp(undefined,undefined) });
     }
-
+    
 }
