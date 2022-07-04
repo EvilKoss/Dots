f1a02c5e29165f1cc113158fc6f7e60c0565cfc2
EViL-KoSS <evil.koss@gmail.com>
2022-07-04 16:07:39 +0300

    finish

--- scripts/script/GameScene.js
+++ scripts/script/GameScene.js
@@ -14,6 +14,7 @@ class GameScene extends Phaser.Scene {
     init() {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.score = 0;
+        this.scoreText = this.add.text(50, 50, this.score,{ font: '40px'}).setOrigin(0.5);
         this.violet2 = {color:0xFF00FF, name:'violet'};
         this.violet = 0xFF00FF;
         this.red = 0xff0000;
@@ -39,22 +40,25 @@ class GameScene extends Phaser.Scene {
         this.click = false;
         this.dots = [];
         this.lines = [];
+        this.tempLines = undefined;
     }
     
     create() {
         this.createTable();
         this.createDots();
+        this.createScoreText();
     }
     
     pledge() {
-        if(this.memoryDict.length < 1) return;
+        if(this.memoryDict.length <= 1) return;
             this.memoryDict.forEach(item => {
             this.table[item.y][item.x].color = 9;
             this.score++;
         });
-        console.log(this.score);
+        
         for(let x = 0; x < this.table.length; x++) {
             while(true) {
+
                 //find hole
                 let hole = -1;
                 for(let y = this.table.length-1; y >= 0; y--) {
@@ -66,6 +70,7 @@ class GameScene extends Phaser.Scene {
                 if(hole < 0) {
                     break;
                 }
+
                 //find ball
                 let ball = -1;
                 for(let y = hole-1; y >= 0; y--) {
@@ -77,10 +82,12 @@ class GameScene extends Phaser.Scene {
                 if(ball < 0) {
                     break;
                 }
+
                 //move ball into hole
                 this.table[hole][x].color = this.table[ball][x].color;
                 this.table[ball][x].color = 9;
             }
+
             //randomize ramaining holes
             for(let y = 0; y < this.table.length && this.table[y][x].color === 9; y++) {
                 this.table[y][x].color = Phaser.Math.Between(0, 4);
@@ -89,6 +96,7 @@ class GameScene extends Phaser.Scene {
         this.memoryDict = [];
         this.dots.forEach(item => {item.destroy()});
         this.lines.forEach(item => {item.destroy()});
+        this.lines = [];
         this.dots = [];
         this.createDots();
     }
@@ -108,26 +116,41 @@ class GameScene extends Phaser.Scene {
     
     onDotsOver(i,j) {
         if(this.click === true){
-            let checkX = this.memoryDict[0].x;
-            let checkY = this.memoryDict[0].y;
-            let checkX2 = this.memoryDict[this.memoryDict.length-1].x;
-            let checkY2 = this.memoryDict[this.memoryDict.length-1].y;
+
+            let checkX = this.memoryDict[this.memoryDict.length-1].x;
+            let checkY = this.memoryDict[this.memoryDict.length-1].y;
+            let checkX2 = j;
+            let checkY2 = i;
             let check = (this.table[i][j].color === this.table[checkY][checkX].color);
             let checkRow = (checkX === checkX2 && checkY === checkY2-1);
             let checkRow2 = (checkX === checkX2 && checkY === checkY2+1);
             let checkRow3 = (checkX === checkX2-1 && checkY === checkY2);
             let checkRow4 = (checkX === checkX2+1 && checkY === checkY2);
+
+            let checkPrevious = false;
             if(this.memoryDict.length >= 2){
-                checkX = this.memoryDict[this.memoryDict.length-2].x;
-                checkY = this.memoryDict[this.memoryDict.length-2].y;
-                console.log([checkRow, checkRow2, checkRow3, checkRow4]);
-                console.log([this.memoryDict[this.memoryDict.length-1],this.memoryDict[this.memoryDict.length-2]]);
+                checkPrevious = (checkX2 === this.memoryDict[this.memoryDict.length-2].x && checkY2 === this.memoryDict[this.memoryDict.length-2].y);
+            }else{
+                checkPrevious = false;
             }
-            if(check){// && (checkRow || checkRow2 || checkRow3 || checkRow4)
+
+            if(check && !checkPrevious && (checkRow || checkRow2 || checkRow3 || checkRow4)){
                 this.memoryDict.push({x: j, y: i});
                 this.drawLine();
-            }else if(!check){// || !(checkRow || checkRow2 || checkRow3 || checkRow4)
+            }
+            else if(checkPrevious){
+                this.lines[this.lines.length-1].destroy();
+                this.lines.pop();
+                this.memoryDict.pop();
+            }
+            else if(checkX === checkX2 && checkY === checkY2){
+
+            }
+            else{
                 this.memoryDict = [];
+                this.lines.forEach(item => {item.destroy()});
+                this.lines = [];
+                this.click = false;
             }
         }
     }
@@ -135,6 +158,11 @@ class GameScene extends Phaser.Scene {
     onDotsUp() {
         this.click = false;
         this.pledge();
+        this.createScoreText();
+        if(this.tempLines != undefined){
+            this.tempLines.destroy();
+            this.tempLines = undefined;
+        }
     }
     
     createDots() {
@@ -153,7 +181,8 @@ class GameScene extends Phaser.Scene {
                 this.table[i][j].height = height;
             }
         }
-        this.input.on("pointerup", () => { this.onDotsUp(undefined,undefined) });
+        this.input.on("pointerup", () => { this.onDotsUp() });
+        this.input.on("pointermove", () => { this.drawLineOnMouseMove() });
     }
 
     drawLine() {
@@ -172,6 +201,33 @@ class GameScene extends Phaser.Scene {
             .closePath()
             .strokePath();
         this.lines.push(graphics);
+    }
+    createScoreText() {
+        this.scoreText.destroy();
+        this.scoreText  = this.add.text(50, 50, this.score,{ font: '40px'}).setOrigin(0.5);
+    };
+
+    drawLineOnMouseMove() {
+        if(!this.click){return}
+        if(this.tempLines != undefined){
+            this.tempLines.destroy();
+            this.tempLines = undefined;
+        }
+
+        let x = game.input.mousePointer.x;
+        let y = game.input.mousePointer.y;
+        let coordinates = this.table[this.memoryDict[this.memoryDict.length-1].y][this.memoryDict[this.memoryDict.length-1].x];
+        let color = this.tableColors[coordinates.color];
+        
+        let graphics = this.add.graphics();
+        graphics.lineStyle(20, color, 1.0)
+            .beginPath()
+            .moveTo(x, y )
+            .lineTo(coordinates.width, coordinates.height)
+            .closePath()
+            .strokePath();
+        this.tempLines = graphics;
+
     }
     
 }
